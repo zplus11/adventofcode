8323d18fd2b7e08c2ac7311c834a7bb057252a79
Naman Taggar <namantaggar.11@gmail.com>
2024-12-02 19:39:21 +0530

    d2

--- 2/solver.m
+++ 2/solver.m
@@ -1,7 +1,6 @@
 (* ::Package:: *)
 
 input=Import[NotebookDirectory[]<>"\\input.txt"];
-tuples=StringSplit[#," "]&/@StringSplit[input,"\n"];
 reports = ToExpression[StringSplit[#," "]&/@StringSplit[input,"\n"]];
 safe[rep_]:=And@@{SubsetQ[{1,2,3},Abs@#],Length[Union[Sign[#]]]==1}&@Differences[rep];
 first=Evaluate[Select[reports,safe[#]&]];
