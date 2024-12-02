d0105633c9149ae29f15445abcb4b462dcf41b68
Naman Taggar <namantaggar.11@gmail.com>
2024-12-02 20:37:27 +0530

    d2

--- 1/solver.m
+++ 1/solver.m
@@ -4,5 +4,7 @@ input=Import[NotebookDirectory[]<>"\\input.txt"];
 tuples=StringSplit[#,"   "]&/@StringSplit[input,"\n"];
 {l,r}=ToExpression@tuples//Transpose;
 Print[Total@Abs[r-l]] (*2066446*)
-count={#,Count[r,#]}&/@l;
-Print[Total[#[[1]]#[[2]]&/@count]] (*24931009*)
+Print[Total[# Count[r,#]&/@l]] (*24931009*)
+
+
+
--- 2/solver.m
+++ 2/solver.m
@@ -4,8 +4,8 @@ input=Import[NotebookDirectory[]<>"\\input.txt"];
 reports = ToExpression[StringSplit[#," "]&/@StringSplit[input,"\n"]];
 safe[rep_]:=And@@{SubsetQ[{1,2,3},Abs@#],Length[Union[Sign[#]]]==1}&@Differences[rep];
 first=Evaluate[Select[reports,safe[#]&]];
-Print[Length@first](*314*)
-Print[Length@Select[Complement[reports,first],Or@@Table[safe[Delete[#,i]],{i,Length@#}]&]+Length@first](*373*)
+Print[Length@first] (*314*)
+Print[Length@Select[Complement[reports,first],Or@@Table[safe[Delete[#,i]],{i,Length@#}]&]+Length@first] (*373*)
 
 
 
